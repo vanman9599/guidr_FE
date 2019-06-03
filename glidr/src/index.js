e65f374423cef75953c16bee3c6e9e10cbd1e3f6
van jordan <vanjordan46@hotmail.com>
2019-06-03 10:48:10 -0700

    initial commit - create index.js

---
+++ glidr/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+
+const store = createStore(
+  reducer, 
+  applyMiddleware(thunk)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
